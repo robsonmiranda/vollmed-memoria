0de0b95e986cb59507114660a29df48b30fa3112
Robson Miranda <rmatosm@gmail.com>
2025-12-14 17:30:57 -0300

    Uso de StringBuffer que é thread safe

--- src/main/java/med/voll/api/testes/memoria/Principal.java
+++ src/main/java/med/voll/api/testes/memoria/Principal.java
@@ -3,8 +3,31 @@ package med.voll.api.testes.memoria;
 import med.voll.api.domain.paciente.Paciente;
 
 public class Principal {
-    public static void main(String[] args) {
-        Paciente paciente = new Paciente();
-        paciente.consultas();
+    public static void main(String[] args) throws InterruptedException {
+        StringBuffer resultado = new StringBuffer();
+
+        var thread1 = new Thread(()->
+        {
+            for(int i = 1; i<100; i++){
+                resultado.append(i);
+                resultado.append(" ");
+            }
+        });
+
+        var thread2 = new Thread(()->
+        {
+            for(int i = 101; i<200; i++){
+                resultado.append(i);
+                resultado.append(" ");
+            }
+        });
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(resultado);
     }
 }
